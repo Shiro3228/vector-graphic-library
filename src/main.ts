4bd52f606e9d40db8cdecd3f2b702925919ec65d
shiro3228 <shiro3228@yandex.ru>
2025-11-25 11:06:50 +0200

    dependency init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile.js";
+import { OSM } from "ol/source.js";
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
