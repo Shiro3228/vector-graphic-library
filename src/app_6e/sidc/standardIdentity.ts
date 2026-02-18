d47669302ef63a1c6531899a76bb24af229a7a92
shiro3228 <shiro3228@yandex.ru>
2026-02-18 15:08:19 +0200

    SidcStandardIdentity added

---
+++ src/app_6e/sidc/standardIdentity.ts
@@ -0,0 +1,11 @@
+enum SidcStandardIdentity {
+    PENDING = 0,
+    UNKNOWN = 1,
+    ASSUMED_FRIEND = 2,
+    FRIEND = 3,
+    NEUTRAL = 4,
+    SUSPECT_OR_JOKER = 5,
+    HOSTILE_OR_FAKER = 6,
+}
+
+export default SidcStandardIdentity;
